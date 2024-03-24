83d568a4353c678c2e8db8e6600b8248d544a759
Saransh Gupta <saranshgupta2112@gmail.com>
2024-03-25 03:13:52 +0530

    auth_name yashika added

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ def get_requirements(file_path:str)-> List[str]:
 setup(
     name ='mlproject',
     version = '0.0.1',
-    author ='Saransh',
+    author ='Saransh_Yashika',
     author_email ='saranshgupta2112@gmail.com',
     packages = find_packages(),
     install_requires = get_requirements('requirements.txt')
